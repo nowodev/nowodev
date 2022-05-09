f38178573dcf601111814bc4c32a429b1d73afe4
Favour <toluwaniokunowo@gmail.com>
2022-05-09 15:28:23 +0100

    add coming_soon page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
     // This starter template is using Vue 3 <script setup> SFCs
     // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-    import Home from "./Home.vue";</script>
+    import Home from "./Home.vue";
+import ComingSoon from "./ComingSoon.vue";</script>
 
 <template>
-    <Home />
+    <!-- <Home /> -->
+    <ComingSoon />
 </template>
