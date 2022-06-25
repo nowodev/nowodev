a3c9cb66f0697e18d60c98c1b7731fe25bc12cef
Favour <toluwaniokunowo@gmail.com>
2022-06-25 22:57:19 +0100

    create components

---
+++ src/components/Button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button
+        class="text-xs px-6 py-2 rounded ring-2 ring-gray-800 hover:bg-gray-800 hover:scale-110 transition-all focus:bg-gray-800 focus:ring focus:ring-gray-800">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+};
+</script>
