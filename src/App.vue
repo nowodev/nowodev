3228f446649aaef5b557a1730a3b4aec692f74c9
Favour <toluwaniokunowo@gmail.com>
2022-08-05 23:03:57 +0100

    install prelineui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-    // This starter template is using Vue 3 <script setup> SFCs
-    // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-    import Home from "./Home.vue";
-import ComingSoon from "./ComingSoon.vue";</script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Home from "./Home.vue";
+</script>
 
 <template>
-    <Home />
-    <!-- <ComingSoon /> -->
+	<Home />
 </template>
